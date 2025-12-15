e50960b3dae357646313f7169b97b718cd0bbd89
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2025-12-15 21:57:22 +0900

    chore: initial commit

---
+++ src/api/kotlin/hiiragi283/core/api/HiiragiCoreAPI.kt
@@ -0,0 +1,5 @@
+package hiiragi283.core.api
+
+data object HiiragiCoreAPI {
+    const val MOD_ID = "hiiragi_core"
+}
---
+++ src/main/kotlin/hiiragi283/core/HiiragiCore.kt
@@ -0,0 +1,10 @@
+package hiiragi283.core
+
+import hiiragi283.core.api.HiiragiCoreAPI
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.fml.ModContainer
+import net.neoforged.fml.common.Mod
+
+@Mod(HiiragiCoreAPI.MOD_ID)
+class HiiragiCore(eventBus: IEventBus, container: ModContainer) {
+}
