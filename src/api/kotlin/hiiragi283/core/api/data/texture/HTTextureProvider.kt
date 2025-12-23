a945b9b79d5238f2dcf04875a5104cb735f8f3c6
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2025-12-23 13:52:12 +0900

    feat: updated HTTextureProvider

--- src/api/kotlin/hiiragi283/core/api/data/texture/HTTextureProvider.kt
+++ src/api/kotlin/hiiragi283/core/api/data/texture/HTTextureProvider.kt
@@ -2,7 +2,12 @@ package hiiragi283.core.api.data.texture
 
 import com.google.common.hash.HashCode
 import com.mojang.blaze3d.platform.NativeImage
+import hiiragi283.core.api.HiiragiCoreAPI
+import hiiragi283.core.api.collection.ImmutableMultiMap
+import hiiragi283.core.api.collection.buildMultiMap
 import hiiragi283.core.api.data.HTDataGenContext
+import hiiragi283.core.api.material.HTMaterialLike
+import hiiragi283.core.api.material.prefix.HTMaterialPrefix
 import net.minecraft.Util
 import net.minecraft.data.CachedOutput
 import net.minecraft.data.DataProvider
@@ -10,10 +15,14 @@ import net.minecraft.data.PackOutput
 import net.minecraft.resources.ResourceLocation
 import net.neoforged.neoforge.client.model.generators.ModelProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
+import java.awt.Color
 import java.io.IOException
 import java.nio.file.Path
 import java.util.concurrent.CompletableFuture
 import java.util.function.BiConsumer
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.iterator
 import kotlin.io.path.inputStream
 
 /**
@@ -80,5 +89,57 @@ abstract class HTTextureProvider(private val packOutput: PackOutput, private val
         val image = NativeImage(other.width, other.height, true)
         image.copyFrom(other)
         return image
+    }
+    
+    protected fun <T : HTMaterialLike> material(
+        output: BiConsumer<ResourceLocation, NativeImage>,
+        pathPrefix: String,
+        materials: Iterable<T>,
+        supportedPrefixes: (T) -> Set<HTMaterialPrefix>,
+        templateFactory: (T, HTMaterialPrefix) -> ResourceLocation?,
+        paletteGetter: (T) -> HTColorPalette,
+    ) {
+        for (material: T in materials) {
+            for (prefix: HTMaterialPrefix in supportedPrefixes(material)) {
+                val templateImage: NativeImage = templateFactory(material, prefix)?.let(::getTexture) ?: continue
+                val image: NativeImage = copyFrom(templateImage)
+
+                for ((index: Int, pixels: Collection<Pair<Int, Int>>) in createTemplate(templateImage).map) {
+                    for ((x: Int, y: Int) in pixels) {
+                        image.setPixelRGBA(x, y, argbToFromABGR(paletteGetter(material)[index].rgb))
+                    }
+                }
+                output.accept(HiiragiCoreAPI.id(pathPrefix, prefix.asPrefixName(), material.asMaterialName()), image)
+            }
+        }
+    }
+
+    protected fun createTemplate(image: NativeImage): ImmutableMultiMap<Int, Pair<Int, Int>> = buildMultiMap {
+        for (x: Int in (0..<image.width)) {
+            for (y: Int in (0..<image.height)) {
+                val color = Color(argbToFromABGR(image.getPixelRGBA(x, y)))
+                val index: Int = getColorIndex(color) ?: continue
+                put(index, x to y)
+            }
+        }
+    }
+
+    private fun getColorIndex(color: Color): Int? = when (color) {
+        Color(0xd8d8d8) -> 0
+        Color(0xb2b2b2) -> 1
+        Color(0x8b8b8b) -> 2
+        Color(0x656565) -> 3
+        Color(0x3e3e3e) -> 4
+        Color(0x181818) -> 5
+        else -> null
+    }
+
+    /**
+     * @see mekanism.common.lib.Color.argbToFromABGR
+     */
+    protected fun argbToFromABGR(argb: Int): Int {
+        val red: Int = argb shr 16 and 0xFF
+        val blue: Int = argb and 0xFF
+        return argb and -0xff0100 or (blue shl 16) or red
     }
 }
--- src/data/kotlin/hiiragi283/core/data/client/HCTextureProvider.kt
+++ src/data/kotlin/hiiragi283/core/data/client/HCTextureProvider.kt
@@ -1,18 +1,13 @@
 package hiiragi283.core.data.client
 
 import com.mojang.blaze3d.platform.NativeImage
-import hiiragi283.core.api.HiiragiCoreAPI
-import hiiragi283.core.api.collection.ImmutableMultiMap
-import hiiragi283.core.api.collection.buildMultiMap
 import hiiragi283.core.api.data.HTDataGenContext
 import hiiragi283.core.api.data.texture.HTTextureProvider
 import hiiragi283.core.api.material.prefix.HTMaterialPrefix
-import hiiragi283.core.common.data.texture.HCMaterialPalette
 import hiiragi283.core.common.material.HCMaterial
 import hiiragi283.core.setup.HCBlocks
 import hiiragi283.core.setup.HCItems
 import net.minecraft.resources.ResourceLocation
-import java.awt.Color
 import java.util.function.BiConsumer
 
 class HCTextureProvider(context: HTDataGenContext) : HTTextureProvider(context) {
@@ -26,37 +21,6 @@ class HCTextureProvider(context: HTDataGenContext) : HTTextureProvider(context) 
         pathPrefix: String,
         transform: (HCMaterial) -> Set<HTMaterialPrefix>,
     ) {
-        for (material: HCMaterial in HCMaterial.entries) {
-            for (prefix: HTMaterialPrefix in transform(material)) {
-                val templateImage: NativeImage = material.getTemplateId(prefix)?.let(::getTexture) ?: continue
-                val image: NativeImage = copyFrom(templateImage)
-
-                for ((index: Int, pixels: Collection<Pair<Int, Int>>) in createTemplate(templateImage).map) {
-                    for ((x: Int, y: Int) in pixels) {
-                        image.setPixelRGBA(x, y, argbToFromABGR(material.colorPalette[index].rgb))
-                    }
-                }
-                output.accept(HiiragiCoreAPI.id(pathPrefix, prefix.asPrefixName(), material.asMaterialName()), image)
-            }
-        }
-    }
-
-    private fun createTemplate(image: NativeImage): ImmutableMultiMap<Int, Pair<Int, Int>> = buildMultiMap {
-        for (x: Int in (0..<image.width)) {
-            for (y: Int in (0..<image.height)) {
-                val color = Color(argbToFromABGR(image.getPixelRGBA(x, y)))
-                val index: Int = HCMaterialPalette.TEMPLATE[color] ?: continue
-                put(index, x to y)
-            }
-        }
-    }
-
-    /**
-     * @see mekanism.common.lib.Color.argbToFromABGR
-     */
-    fun argbToFromABGR(argb: Int): Int {
-        val red = argb shr 16 and 0xFF
-        val blue = argb and 0xFF
-        return argb and -0xff0100 or (blue shl 16) or red
+        material(output, pathPrefix, HCMaterial.entries, transform, HCMaterial::getTemplateId, HCMaterial::colorPalette)
     }
 }
